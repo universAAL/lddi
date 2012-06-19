5e48d10c49d9e3da871f2d1df35a3b00c82ffb1a
DI(FH) Thomas Fuxreiter <thomas.fuxreiter@ait.ac.at>
2012-06-19 11:17:25 +0000

    upgrade of all LDDI artifacts to 0.0.4

--- knx.devicemanager/src/main/java/org/universAAL/knx/devicemanager/util/LogTracker.java
+++ knx.devicemanager/src/main/java/org/universAAL/knx/devicemanager/util/LogTracker.java
@@ -14,7 +14,7 @@ public class LogTracker extends ServiceTracker implements LogService {
 	@Override
 	public Object addingService(ServiceReference reference) {
 		Object ret = super.addingService(reference);
-		System.out.println("stdout: [KNX.DEVICEMANAGER] LogService added!");
+//		System.out.println("stdout: [KNX.DEVICEMANAGER] LogService added!");
 		log(LOG_DEBUG,"LogService added!");
 		return ret;
 	}
@@ -22,14 +22,14 @@ public class LogTracker extends ServiceTracker implements LogService {
 	@Override
 	public void modifiedService(ServiceReference reference, Object service) {
 		super.modifiedService(reference, service);
-		System.out.println("stdout: [KNX.DEVICEMANAGER] LogService modified!");
+//		System.out.println("stdout: [KNX.DEVICEMANAGER] LogService modified!");
 		log(LOG_DEBUG,"LogService modified!");
 	}
 
 	@Override
 	public void removedService(ServiceReference reference, Object service) {
 		super.removedService(reference, service);
-		System.out.println("stdout: [KNX.DEVICEMANAGER] LogService removed!");
+//		System.out.println("stdout: [KNX.DEVICEMANAGER] LogService removed!");
 		log(LOG_DEBUG,"LogService removed!");
 	}
 
