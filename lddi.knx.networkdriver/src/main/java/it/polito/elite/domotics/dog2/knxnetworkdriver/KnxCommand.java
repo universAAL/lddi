cc3d96741109d9b3cd7031dba637e2c9147052b0
DI(FH) Thomas Fuxreiter <thomas.fuxreiter@ait.ac.at>
2012-06-27 15:41:30 +0000

    add lddi prefix

---
+++ lddi.knx.networkdriver/src/main/java/it/polito/elite/domotics/dog2/knxnetworkdriver/KnxCommand.java
@@ -0,0 +1,78 @@
+package it.polito.elite.domotics.dog2.knxnetworkdriver;
+
+import org.universAAL.lddi.knx.utils.KnxEncoder;
+
+
+/***
+ * It stores information about Knx commands 
+ * @author Emiliano
+ *
+ */
+public class KnxCommand {
+	
+	String commandName;
+	String groupAddress;
+	String hexValue;
+	
+	
+
+	
+
+	public KnxCommand(String commandName, String groupAddress, String hexValue) {
+		this.commandName=commandName;
+		this.groupAddress=groupAddress;
+		this.hexValue=hexValue;
+	}
+
+
+
+
+
+	public KnxCommand(String notificationName, String groupAddress2) {
+		this.commandName=notificationName;
+		this.groupAddress=groupAddress2;
+		this.hexValue="";
+	}
+
+
+
+
+
+	public String getName() {
+		return commandName;
+	}
+
+
+
+
+
+	public String getGroupAddress() {
+		return groupAddress;
+	}
+
+
+
+
+
+	public String getHexValue() {
+		return hexValue;
+	}
+
+
+
+
+    /**
+     * Check if a command or a notification is compatible with the passed device and hex value
+     * @param device
+     * @param status
+     * @return
+     */
+	public boolean compatibleWith(String device, String status) {
+		
+		return this.groupAddress.equalsIgnoreCase(device) && status.equalsIgnoreCase(KnxEncoder.clearHexValue(this.hexValue));
+	}
+
+	
+	
+	
+}
---
+++ lddi.knx.networkdriver/src/main/java/it/polito/elite/domotics/dog2/knxnetworkdriver/KnxNotification.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package it.polito.elite.domotics.dog2.knxnetworkdriver;
+
+import org.universAAL.lddi.knx.utils.KnxEncoder;
+
+/**
+ * This class stores information about a Knx notification
+ * @author Castellina Emiliano
+ *
+ */
+public class KnxNotification extends KnxCommand {
+
+	/**
+	 * @param commandName
+	 * @param groupAddress
+	 * @param hexValue
+	 */
+	public KnxNotification(String commandName, String groupAddress,
+			String hexValue) {
+		super(commandName, groupAddress, hexValue);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param notificationName
+	 * @param groupAddress2
+	 */
+	public KnxNotification(String notificationName, String groupAddress2) {
+		super(notificationName, groupAddress2);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public String getAddressHex(){
+		return this.groupAddress+"#"+KnxEncoder.clearHexValue(hexValue);
+	}
+	
+	
+
+}
