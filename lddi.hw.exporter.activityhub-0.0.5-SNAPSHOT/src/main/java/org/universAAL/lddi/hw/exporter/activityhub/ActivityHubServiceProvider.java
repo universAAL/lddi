570495c822038f9928b953112922ab5ab0f473ea
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-14 14:03:23 +0000

    Automatic tag of lddi.hw.exporter.activityhub version: 0.0.5-SNAPSHOT

---
+++ lddi.hw.exporter.activityhub-0.0.5-SNAPSHOT/src/main/java/org/universAAL/lddi/hw/exporter/activityhub/ActivityHubServiceProvider.java
@@ -0,0 +1,375 @@
+package org.universAAL.lddi.hw.exporter.activityhub;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import org.osgi.service.log.LogService;
+import org.universAAL.lddi.iso11073.activityhub.location.ActivityHubLocationUtil.ActivityHubLocation;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.ServiceCall;
+import org.universAAL.middleware.service.ServiceCallee;
+import org.universAAL.middleware.service.ServiceResponse;
+import org.universAAL.middleware.service.owls.process.ProcessOutput;
+import org.universAAL.middleware.service.owls.profile.ServiceProfile;
+import org.universAAL.ontology.activityhub.*;
+import org.universAAL.ontology.activityhub.factory.ActivityHubFactory;
+import org.universAAL.ontology.location.indoor.Room;
+
+/**
+ * uAAL service provider (service bus) for the ActivityHub
+ * no ontology definitions here!
+ * 
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class ActivityHubServiceProvider extends ServiceCallee {
+
+	ActivityHubBusServer theServer;
+	private LogService logger;
+
+	static final String LOCATION_URI_PREFIX = "urn:aal_space:myHome#";
+	static final String ACTIVITYHUB_LOCATION_PREFIX = "MDC_AI_LOCATION_";
+
+	private static ActivityHubFactory factory = new ActivityHubFactory();
+
+	/**
+	 * Constructor
+	 * 
+	 * @param context
+	 * @param realizedServices
+	 */
+	public ActivityHubServiceProvider(ModuleContext mc, ActivityHubBusServer busServer) {
+		/** register my services on uAAL service bus */
+		super(mc, ActivityHubServiceOntology.profiles);
+
+		this.theServer = busServer;
+		this.logger = busServer.getLogger();
+
+		this.logger.log(LogService.LOG_INFO, ActivityHubServiceOntology.profiles.length + 
+				" ActivityHubServer services registered on uAAL service bus:" + getserviceNames() );
+	}
+
+
+	// this is just to prepare a standard error message for later use
+    private static final ServiceResponse invalidInput = new ServiceResponse(
+	    CallStatus.serviceSpecificFailure);
+    private static final ServiceResponse internalError = new ServiceResponse(
+    	    CallStatus.serviceSpecificFailure);
+    
+    static {
+    	invalidInput.addOutput(new ProcessOutput(
+    			ServiceResponse.PROP_SERVICE_SPECIFIC_ERROR, "Invalid input!"));
+    	internalError.addOutput(new ProcessOutput(
+    			ServiceResponse.PROP_SERVICE_SPECIFIC_ERROR, "Internal Error!"));
+    }
+    
+
+	/**
+	 * create a service response with all available ActivityHub sensors
+	 * 
+	 * @return ServiceResponse with output objects 
+	 */
+	private ServiceResponse getControlledActivityHubSensors() {
+
+		ServiceResponse sr = null;
+//		try {
+			// create a list including the available sensors and sensor types
+			Hashtable<String,Integer> sensorList = new Hashtable<String,Integer>();
+
+			// fetch data from my server
+			theServer.getActivityHubSensorList(sensorList);
+
+//			LogUtils.logInfo(Activator.moduleContext, ActivityHubServiceProvider.class,
+//					"getControlledActivityHubSensors",
+//					new Object[] { "sensorList size: " + sensorList.size() }, null);
+			
+			// return if no sensors available
+			if (sensorList.size() == 0) {
+				ServiceResponse errorResponse = new ServiceResponse(CallStatus.serviceSpecificFailure);
+				errorResponse.addOutput(new ProcessOutput(
+		    			ServiceResponse.PROP_SERVICE_SPECIFIC_ERROR, "No ActivityHub Sensors available!"));
+				return errorResponse;
+			}
+			
+			// prepare array for service output
+			ArrayList activityHubSensorList = new ArrayList(sensorList.size());
+
+			// iterate through sensor list and add sensors to output array
+			Iterator<Entry<String, Integer>> it = sensorList.entrySet().iterator();
+			while (it.hasNext()) {
+				Entry<String, Integer> entry = it.next();
+
+				ActivityHubSensor ahs = (ActivityHubSensor) factory.createInstance(null, 
+						constructActivityHubSensorURIfromLocalID(entry.getKey()),
+						entry.getValue().intValue());
+				// ahs.setLocation ??
+
+				this.logger.log(LogService.LOG_INFO, "ActivityHubSensor URI: " + ahs.getURI() +
+						" sensorType: " + ahs.getSensorType());
+				
+//				LogUtils.logInfo(Activator.moduleContext, ActivityHubServiceProvider.class,
+//						"getControlledActivityHubSensors",
+//						new Object[] { "ActivityHubSensor URI: " + ahs.getURI() +
+//							" sensorType: " + ahs.getSensorType() }, null);
+				
+				activityHubSensorList.add(ahs);
+			}
+
+			sr = new ServiceResponse(CallStatus.succeeded);
+
+			// create and add a ProcessOutput-Event that binds the output URI to the
+			// created list of sensors
+			sr.addOutput(new ProcessOutput(
+					ActivityHubServiceOntology.OUTPUT_CONTROLLED_ACTIVITYHUB_SENSORS, activityHubSensorList));
+//			return sr;
+			
+//		} catch (Exception e) {
+//			LogUtils.logInfo(Activator.moduleContext, ActivityHubServiceProvider.class,
+//					"getControlledActivityHubSensors",
+//					new Object[] { "ERROR on service response: " + e.getMessage() }, null);
+//			e.printStackTrace();
+//			sr = internalError;
+//		}
+//		sr.getOutput(paramURI, true);
+		return sr;
+	}
+	
+    private static String constructActivityHubSensorURIfromLocalID(String localID) {
+    	return ActivityHubServiceOntology.DEVICE_URI_PREFIX + localID;
+    }
+	
+	/**
+	 * create a service response with information about the requested ActivityHub device
+	 * 
+	 * @param activityHubDeviceURI
+	 * @return ServiceResponse with output objects 
+	 */
+	private ServiceResponse getActivityHubDeviceInfo(ActivityHubSensor activityHubSensor) {
+//		this.logger.log(LogService.LOG_INFO, "Service called: getActivityHubDeviceInfo" +
+//				" for sensorURI: " + activityHubSensor.getURI());
+		
+	    LogUtils.logDebug(Activator.moduleContext, ActivityHubServiceProvider.class, "getActivityHubDeviceInfo",
+			    new Object[] { "Service called: getActivityHubDeviceInfo" +
+			" for sensorURI: " + activityHubSensor.getURI()},null);
+
+		try {
+			// collect the needed data
+			String deviceId = extractLocalIDfromdeviceUri(activityHubSensor.getURI());
+			
+		    LogUtils.logDebug(Activator.moduleContext, ActivityHubServiceProvider.class, "getActivityHubDeviceInfo",
+				    new Object[] { "extracted deviceId: " + deviceId },null);
+
+//			this.logger.log(LogService.LOG_INFO, "extracted deviceId: " + deviceId);
+			
+			// check device
+			if ( !theServer.validateDevice(deviceId) ) {
+				// no such device !
+			    LogUtils.logError(Activator.moduleContext, ActivityHubServiceProvider.class, "getActivityHubDeviceInfo",
+					    new Object[] { "no such device found! " + deviceId },null);
+//				this.logger.log(LogService.LOG_ERROR, "no such device found! " + deviceId);
+				ServiceResponse sr = new ServiceResponse(CallStatus.serviceSpecificFailure);
+				return sr;
+			}
+			
+		    LogUtils.logInfo(Activator.moduleContext, ActivityHubServiceProvider.class, "getActivityHubDeviceInfo",
+				    new Object[] { "matching device found for " + deviceId },null);
+//			this.logger.log(LogService.LOG_INFO, "matching device found for " + deviceId);
+
+			ActivityHubLocation loc = theServer.getDeviceLocation(deviceId);
+			
+			// which format needed for event?
+			int lastDeviceEvent = theServer.getLastDeviceEvent(deviceId);
+			
+			ActivityHubSensorEvent ahse = createSensorEvent(lastDeviceEvent, activityHubSensor);
+			if (ahse == null) {
+			    LogUtils.logError(Activator.moduleContext, ActivityHubServiceProvider.class, "getActivityHubDeviceInfo",
+					new Object[] { "Could not create a SensorEvent object for: " + 
+			    	activityHubSensor.getURI() + " and sensor value: " + lastDeviceEvent },null);
+//				this.logger.log(LogService.LOG_ERROR, "Could not create a SensorEvent object " +
+//						"for: " + activityHubSensor.getURI() + " and sensor value: " + lastDeviceEvent); 
+				ServiceResponse sr = new ServiceResponse(CallStatus.serviceSpecificFailure);
+				return sr;
+			}
+
+			// We assume that the Service-Call always succeeds
+			ServiceResponse sr = new ServiceResponse(CallStatus.succeeded);
+
+			// create and add a ProcessOutput-Event that binds the output URI to
+			// the last event of the device
+			sr.addOutput(new ProcessOutput(
+					ActivityHubServiceOntology.OUTPUT_SENSOR_MEASUREMENT, ahse));
+			
+			
+			// create and add a ProcessOutput-Event that binds the output URI to
+			// the location of the device
+			// loc maybe null !!
+			sr.addOutput(new ProcessOutput(
+					ActivityHubServiceOntology.OUTPUT_SENSOR_LOCATION, new Room(
+							constructLocationURIfromLocalID(loc))));
+			return sr;
+			
+		} catch (Exception e) {
+			return invalidInput;
+		}
+	}
+
+    /**
+     * Creates sensor event object from ontology (from enums).
+     * The abstract class ActivityHubSensorEvent is used here.
+     * 
+	 * @param lastSensorEvent
+	 * @return sensor specific event object from ontology
+	 */
+	private ActivityHubSensorEvent createSensorEvent(int lastSensorEvent, ActivityHubSensor input) {
+
+		/** list of sensors: org.universAAL.ontology.activityhub.util.ActivityHubSensorType */
+		if (input instanceof FallSensor)
+			return FallSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof PersSensor)
+			return PersSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof SmokeSensor)
+			return EnvironmentalSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof CoSensor)
+			return EnvironmentalSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof WaterSensor)
+			return EnvironmentalSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof GasSensor)
+			return EnvironmentalSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof MotionSensor)
+			return MotionSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof PropertyExitSensor)
+			return PropertyExitSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof EnuresisSensor)
+			return EnuresisSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof ContactClosureSensor)
+			return ContactClosureSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof UsageSensor)
+			return UsageSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof SwitchSensor)
+			return SwitchSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof MedicationDosageSensor)
+			return MedicationDosageSensorEvent.getEventByOrder(lastSensorEvent);
+		else if (input instanceof TemperatureSensor)
+			return TemperatureSensorEvent.getEventByOrder(lastSensorEvent);
+		
+	    LogUtils.logError(Activator.moduleContext, ActivityHubServiceProvider.class, "createSensorEvent",
+			new Object[] { "No matching ActivityHubSensorType found for service bus call: " + 
+			input.getURI() }, null);
+		
+//		this.logger.log(LogService.LOG_ERROR, "No matching ActivityHubSensor found for service bus call: " + 
+//				input.toString());
+		return null;
+	}
+
+	private static String extractLocalIDfromdeviceUri(String deviceUri) {
+    	return deviceUri.substring(ActivityHubServiceOntology.DEVICE_URI_PREFIX.length());
+    }
+
+	/**
+	 * If param loc is null "unknown" is returned
+	 * @param loc
+	 * @return URI of room
+	 */
+    private static String constructLocationURIfromLocalID(ActivityHubLocation loc) {
+    	if ( loc == null ) return LOCATION_URI_PREFIX + "unknown"; 
+    	else return LOCATION_URI_PREFIX + loc.toString().substring(ACTIVITYHUB_LOCATION_PREFIX.length());
+    }
+
+
+	/**
+	 * handle service request from uAAL service bus
+	 * @see org.universAAL.middleware.service.ServiceCallee#handleCall(org.universAAL.middleware.service.ServiceCall)
+	 */
+	@Override
+	public ServiceResponse handleCall(ServiceCall call) {
+//	    LogUtils.logDebug(Activator.moduleContext, ActivityHubServiceProvider.class, "handleCall",
+//			    new Object[] { "I'm here" }, null);
+	    
+		if (call == null)
+		    return null;
+
+		String operation = call.getProcessURI();
+		if (operation == null)
+		    return null;
+
+		
+	    LogUtils.logDebug(Activator.moduleContext, ActivityHubServiceProvider.class, "handleCall",
+			    new Object[] { "operation: " + operation }, null);
+
+		
+		/** operations without input */
+		if (operation.startsWith(ActivityHubServiceOntology.SERVICE_GET_CONTROLLED_ACTIVITYHUB_SENSORS))
+			    return getControlledActivityHubSensors();
+		
+		
+		/** following operations all need input value */
+		Object input = call.getInputValue(ActivityHubServiceOntology.INPUT_SENSOR_URI);
+		if (input == null) {
+		    LogUtils.logWarn(Activator.moduleContext, ActivityHubServiceProvider.class, "handleCall",
+				    new Object[] { "Incoming call but input is missing!" }, null);
+			return invalidInput;
+		}
+
+		//input sollte ein ActivityHubSensor sein
+		
+	    LogUtils.logDebug(Activator.moduleContext, ActivityHubServiceProvider.class, "handleCall",
+			    new Object[] { "Incoming call for: " + ((ActivityHubSensor)input).getURI() }, null);
+
+		if (operation.startsWith(ActivityHubServiceOntology.SERVICE_GET_ACTIVITYHUB_SENSOR_INFO)) {
+			return getActivityHubDeviceInfo((ActivityHubSensor)input);
+		}
+		    
+		// no match
+		return null;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.universAAL.middleware.service.ServiceCallee#communicationChannelBroken()
+	 */
+	@Override
+	public void communicationChannelBroken() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+    /**
+     * provide names (URI) of service profiles 
+	 * @return
+	 */
+	private String getserviceNames() {
+		StringBuffer serviceNames = new StringBuffer();
+		for ( ServiceProfile prof : ActivityHubServiceOntology.profiles ) {
+			serviceNames.append("\n" 
+//					prof.getServiceName() + "; " + //null
+//					prof.getNamespace() + "; " + //null
+//					prof.getNumberOfInputs() + "; " + //0
+//					+ prof.getProcessURI() + "; " //http://ontology.universAAL.org/ActivityHubServer.owl#getControlledActivityHubSensorsProcess
+//					prof.getType()+ "; " + //http://www.daml.org/services/owl-s/1.1/Profile.owl#Profile
+//					prof.getURI()+ "; " + //urn:anonymous:_:c0a83c6bc8d08bb4:457
+//					prof.getTheService().MY_URI + "; " //http://www.daml.org/services/owl-s/1.1/Service.owl#Service
+//					+ prof.PROP_OWLS_PROFILE_SERVICE_NAME + "; " //http://www.daml.org/services/owl-s/1.1/Profile.owl#serviceName
+//					+ prof.MY_URI + "; " //http://www.daml.org/services/owl-s/1.1/Profile.owl#Profile
+//					+ prof.OWLS_PROFILE_NAMESPACE + "; " //http://www.daml.org/services/owl-s/1.1/Profile.owl#
+					
+					// we have a winner !
+					+ prof.getTheService().getURI() //http://ontology.universAAL.org/ActivityHubServer.owl#getControlledActivityHubSensors
+					
+//					+ prof.getTheService().getClassURI()+ "; "  //http://ontology.universAAL.org/ActivityHub.owl#ActivityHub
+																
+//					
+//					// also good!
+//					+ prof.getTheService().getLocalName()+ "; " //getControlledActivityHubSensors
+//					
+//					+ prof.getTheService().MY_URI+ "; " //http://www.daml.org/services/owl-s/1.1/Service.owl#Service
+
+//					+ " and"
+			);
+		}
+		return serviceNames.toString();
+	}
+}
---
+++ lddi.hw.exporter.activityhub-0.0.5-SNAPSHOT/src/main/java/org/universAAL/lddi/iso11073/activityhub/driver/Iso11073ContactClosureSensorInstance.java
@@ -0,0 +1,119 @@
+package org.universAAL.lddi.iso11073.activityhub.driver;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.device.Constants;
+import org.osgi.service.log.LogService;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.universAAL.lddi.iso11073.activityhub.devicecategory.Iso11073ContactClosureSensor;
+import org.universAAL.lddi.iso11073.activityhub.devicemodel.ContactClosureSensor;
+import org.universAAL.lddi.iso11073.activityhub.devicemodel.ContactClosureSensorEvent;
+import org.universAAL.lddi.iso11073.activityhub.driver.interfaces.ActivityHubDriver;
+import org.universAAL.lddi.iso11073.activityhub.driver.interfaces.ActivityHubDriverClient;
+
+/**
+ * Working instance of the ActivityHub ContactClosure driver.
+ * Tracks on the ContactClosureSensor device service passed in the attach method 
+ * in Iso11073ContactClosureSensorDriver class.
+ * This instance is passed to the consuming client (e.g. uAAL exporter bundle).
+ * When the ContactClosureSensor device service disappears, this driver is removed
+ * from the consuming client and from the device.
+ * 
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class Iso11073ContactClosureSensorInstance extends ActivityHubDriver 
+	implements Iso11073ContactClosureSensor ,ServiceTrackerCustomizer, Constants {
+
+	private BundleContext context;
+	private LogService logger;
+
+//	private ContactClosureSensorEvent lastSensorEvent;
+	
+	/**
+	 * @param c OSGi BundleContext
+	 * @param sr Service reference of ISO device
+	 * @param client Link to consumer of this driver (e.g. uAAL exporter bundle)
+	 */
+	public Iso11073ContactClosureSensorInstance(BundleContext c,  
+			ActivityHubDriverClient client, LogService log) {
+		super(client);
+
+		this.context=c;
+		this.logger=log;
+	}
+
+	
+	/* (non-Javadoc)
+	 * @see org.universAAL.iso11073.activityhub.driver.interfaces.ActivityHubDriver#getLastSensorEvent()
+	 */
+	@Override
+	public int getLastSensorEvent() {
+		return this.device.getSensorEventValue();
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.osgi.util.tracker.ServiceTrackerCustomizer#addingService(org.osgi.framework.ServiceReference)
+	 */
+	/**
+	 * @param ActivityHubSensor service
+	 */
+	public Object addingService(ServiceReference reference) {
+		
+		ContactClosureSensor ccs = (ContactClosureSensor) this.context.getService(reference);
+		
+		/** now couple my driver to the device */
+		if ( this.setDevice(ccs) )
+			this.logger.log(LogService.LOG_INFO, "Successfully coupled " + Iso11073ContactClosureSensor.MY_DEVICE_CATEGORY 
+					+ " driver to device " + this.device.getDeviceId());
+		else {
+			this.logger.log(LogService.LOG_ERROR, "Error coupling " + Iso11073ContactClosureSensor.MY_DEVICE_CATEGORY
+					+ " driver to device " + this.device.getDeviceId() + ". No appropriate " +
+					"ISO device created!");
+			return null;
+		}
+		
+		//return null; JavaDoc: @return The service object to be tracked for the ServiceReference object or null if the ServiceReference object should not be tracked.
+		return ccs;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.osgi.util.tracker.ServiceTrackerCustomizer#modifiedService(org.osgi.framework.ServiceReference, java.lang.Object)
+	 */
+	public void modifiedService(ServiceReference reference, Object service) {
+		this.logger.log(LogService.LOG_INFO, "Tracked ActivityHub device service was modified. " +
+				"Going to update the Iso11073ContactClosureSensorInstance");
+		removedService(reference, service);
+		addingService(reference);		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.osgi.util.tracker.ServiceTrackerCustomizer#removedService(org.osgi.framework.ServiceReference, java.lang.Object)
+	 */
+	public void removedService(ServiceReference reference, Object service) {
+		// removed device service
+		this.context.ungetService(reference);
+		this.detachDriver();
+		this.removeDriver();
+	}
+
+	/**
+	 * forward event to client
+	 */
+	public void incomingSensorEvent(int event) {
+
+		// TODO send event to client
+		this.logger.log(LogService.LOG_INFO, "Driver " + Iso11073ContactClosureSensor.MY_DEVICE_CATEGORY +
+				" for device " + this.device.getDeviceId() + " received new event " + 
+				ContactClosureSensorEvent.getContactClosureSensorEvent(event).toString());
+
+		try {
+			this.client.incomingSensorEvent(this.device.getDeviceId(), this.device.getDeviceCategory(), event);
+		} catch (AssertionError ae) {
+			this.logger.log(LogService.LOG_ERROR, "No suitable ContactClosureSensorEvent found " +
+					"for value: " +	event);
+			ae.printStackTrace();
+		}
+	}
+
+}
