b404b7de6039a988d18351b0a6976d7f3ea9ec84
DI(FH) Thomas Fuxreiter <thomas.fuxreiter@ait.ac.at>
2012-12-18 15:50:22 +0000

    Version update to 1.3.2-SNAPSHOT

--- lddi.knx.exporter/src/test/java/org/universAAL/lddi/knx/driver/KnxDpt9InstanceTest.java
+++ lddi.knx.exporter/src/test/java/org/universAAL/lddi/knx/driver/KnxDpt9InstanceTest.java
@@ -14,7 +14,7 @@ public class KnxDpt9InstanceTest {
 	@Test
 	public void calculateFloatValueTest() {
 		KnxDpt9Instance d = new KnxDpt9Instance(null, null, null);
-		float fl = d.calculateFloatValue( new byte[] {(byte) 0x80,(byte) 0x0C,(byte) 0xA6} );
+		float fl = d.calculateFloatValue( new byte[] {(byte) 0x0C,(byte) 0xA6} );
 		assertTrue("KnxDpt9Device.calculateFloatValue( new byte[] {(byte) 0x0C,(byte) 0xA6} ) " +
 				"returns " + fl + " but should return 23.8", 
 				Float.toString(fl).equalsIgnoreCase("23.8"));
