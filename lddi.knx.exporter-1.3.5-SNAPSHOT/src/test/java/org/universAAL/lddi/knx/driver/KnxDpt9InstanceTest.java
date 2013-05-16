a105ddb7fffde25b6c5e51b900b3be0f3f44ea2a
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-16 15:25:22 +0000

    Automatic tag of lddi.knx.exporter version: 1.3.5-SNAPSHOT

---
+++ lddi.knx.exporter-1.3.5-SNAPSHOT/src/test/java/org/universAAL/lddi/knx/driver/KnxDpt9InstanceTest.java
@@ -0,0 +1,22 @@
+package org.universAAL.lddi.knx.driver;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * 
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class KnxDpt9InstanceTest {
+
+	@Test
+	public void calculateFloatValueTest() {
+		KnxDpt9Instance d = new KnxDpt9Instance(null, null, null);
+		float fl = d.calculateFloatValue( new byte[] {(byte) 0x0C,(byte) 0xA6} );
+		assertTrue("KnxDpt9Device.calculateFloatValue( new byte[] {(byte) 0x0C,(byte) 0xA6} ) " +
+				"returns " + fl + " but should return 23.8", 
+				Float.toString(fl).equalsIgnoreCase("23.8"));
+	}
+}
