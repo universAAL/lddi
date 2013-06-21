aa0535682b75baaedc7ae184d512008fa572d8da
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 15:45:18 +0000

    Automatic tag of lddi.knx.networkdriver version: 2.0.0

---
+++ lddi.knx.networkdriver/src/main/java/org/universAAL/lddi/knx/networkdriver/KnxWriter.java
@@ -0,0 +1,166 @@
+package org.universAAL.lddi.knx.networkdriver;
+
+import java.net.*;
+
+import org.osgi.service.log.LogService;
+import org.universAAL.lddi.knx.utils.KnxCommand;
+import org.universAAL.lddi.knx.utils.KnxEncoder;
+
+/**
+ * Envelopes KNX commands to UDP Packets and sends them on a UDP Multicast Channel.
+ * Uses KNXEncoder to operate translation from high-level commands to low-level KNX commands.
+ * 
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class KnxWriter {
+
+	protected KnxNetworkDriverImp core;
+	private String lastDeviceAddress;
+	private boolean lastDeviceStatus;
+	private KnxCommand lastCommandType;
+	private boolean repeatBit = false;
+	private volatile byte[] lastPacketSent;
+
+	public KnxWriter(KnxNetworkDriverImp core) {
+		this.core = core;
+	}
+
+	/**
+	 * @return the lastPacketSent
+	 */
+	public byte[] getLastPacketSent() {
+		return lastPacketSent;
+	}
+
+	/**
+	 * Convert byte array to hex encoded string without delimiters.
+	 */
+	public String convertToHex(byte[] b) {
+		StringBuilder byteString = new StringBuilder();
+
+		for (int i = 0; i < b.length; i++) {
+
+			String hexNumber = "0" + Integer.toHexString(0xff & b[i]);
+
+			byteString.append(hexNumber);
+
+		}
+		return byteString.toString();
+	}
+
+	/**
+	 * Wrapper for write method. Without command type.
+	 */
+	public void write(String deviceAddress, boolean deviceStatus) {
+		write(deviceAddress, deviceStatus, KnxCommand.VALUE_WRITE);
+	}
+
+	/**
+	 * Send KNX command to UPD multichannel. Store current sent telegram.
+	 * 
+	 * @param deviceAddress
+	 *            knx group address (1/2/3)
+	 * @param deviceStatus
+	 *            knx command
+	 * @param commandType
+	 */
+	public void write(String deviceAddress, boolean deviceStatus,
+			KnxCommand commandType) {
+		if (this.lastDeviceAddress.equals(deviceAddress)
+				&& this.lastDeviceStatus == deviceStatus
+				&& this.lastCommandType == commandType) {
+			// same command as last time; set Repeat-Bit
+			this.repeatBit = false;
+		}
+
+		try {
+			// Connection
+			DatagramSocket sender = new DatagramSocket();
+
+			// FOCUS: command to host
+			// InetAddress addr = InetAddress.getByName(core.getHouseIp());
+
+			// FOCUS: command to multicast
+			InetAddress addr = InetAddress.getByName(core.getMulticastIp());
+
+			// Translating commands from String to byte[]
+			byte[] telegram = KnxEncoder.encode(repeatBit, deviceAddress,
+					deviceStatus, commandType);
+
+			// Generating UDP packet
+			DatagramPacket packet = new DatagramPacket(telegram,
+					telegram.length, addr, core.getMulticastUdpPort());
+
+			// Sending the packet
+			core.getLogger().log(
+					LogService.LOG_INFO,
+					"Sending command to KNX: "
+							+ KnxEncoder.convertToReadableHex(telegram));
+			sender.send(packet);
+			// store last sent command
+			this.lastPacketSent = KnxEncoder.removeTrailingZeros(telegram);
+			sender.close();
+			this.lastDeviceAddress = deviceAddress;
+			this.lastDeviceStatus = deviceStatus;
+			this.lastCommandType = commandType;
+
+		} catch (Exception e) {
+			core.getLogger().log(LogService.LOG_ERROR,
+					"Unable to write to the server KNX... " + e);
+		} finally {
+			this.repeatBit = false;
+		}
+
+	}
+
+	/**
+	 * Send KNX command 00 to group Address. Is seems that all devices belonging
+	 * to this group address answer with their status. So, multiple answer
+	 * telegrams are possible.
+	 * 
+	 * @param knx address; either group address (1/2/3) or device address (1.2.3)
+	 */
+	public void requestDeviceStatus(String deviceId) {
+		try {
+			// Connection
+			DatagramSocket sender = new DatagramSocket();
+
+			// FOCUS: command to host
+			// InetAddress addr = InetAddress.getByName(core.getHouseIp());
+
+			// FOCUS: command to multicast
+			InetAddress addr = InetAddress.getByName(core.getMulticastIp());
+
+			// Translating commands from String to byte[]
+			// adding 00 for status and READ as message type
+			byte[] telegram = KnxEncoder.encode(deviceId, false,
+					KnxCommand.VALUE_READ);
+
+			// Generating UDP packet
+			DatagramPacket packet = new DatagramPacket(telegram,
+					telegram.length, addr, core.getMulticastUdpPort());
+
+			// Sending the packet
+			core.getLogger().log(LogService.LOG_INFO,
+					"Requesting status from KNX device " + deviceId);
+			sender.send(packet);
+			core.getLogger().log(
+					LogService.LOG_DEBUG,
+					"Sending KNX command "
+							+ KnxEncoder.convertToReadableHex(telegram));
+
+			this.lastPacketSent = KnxEncoder.removeTrailingZeros(telegram);
+			// core.getLogger().log(LogService.LOG_DEBUG,
+			// "lastPacketSent " +
+			// KnxEncoder.getHexString(this.lastPacketSent));
+			// packet = new DatagramPacket(telegram, telegram.length, addr,
+			// 51000);
+
+			sender.close();
+
+		} catch (Exception e) {
+			core.getLogger().log(LogService.LOG_ERROR,
+					"Unable to write to KNX bus " + e);
+		}
+	}
+}
---
+++ lddi.knx.networkdriver/src/main/java/org/universAAL/lddi/knx/networkdriver/util/KnxShellCommand.java
@@ -0,0 +1,39 @@
+package org.universAAL.lddi.knx.networkdriver.util;
+
+
+import org.apache.felix.service.command.Descriptor;
+import org.universAAL.lddi.knx.networkdriver.KnxNetworkDriverImp;
+import org.universAAL.lddi.knx.utils.KnxCommand;
+
+/**
+ * Provide Gogo shell commands.
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class KnxShellCommand {
+
+	private KnxNetworkDriverImp nwDriver;
+
+	public KnxShellCommand(KnxNetworkDriverImp networkDriver) {
+		this.nwDriver = networkDriver;
+	}
+
+	/**
+	 * Provide Gogo shell command: 'knxcommand'
+	 * @param knx group address
+	 * @param command
+	 * @param command type
+	 */
+    @Descriptor("send knx command to knx bus")
+    public void knxcommand(
+    		@Descriptor("group address (1/2/3)") String ga,
+    		@Descriptor("on/off command (0 or 1)") String command,
+    		@Descriptor("command type: read or write  (r or w)") String commandType) {
+    	
+    	KnxCommand type = KnxCommand.VALUE_WRITE;
+    	if (commandType.equals("r")) type = KnxCommand.VALUE_READ;
+    	
+    	if (command.equals("1")) nwDriver.sendCommand(ga, true, type);
+    	else nwDriver.sendCommand(ga, false, type);
+
+    }
+}
