7abcc0603c93747b91e6ddaf258840670b82a394
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 15:37:02 +0000

    Automatic tag of lddi.iso11073.library version: 1.3.6-SNAPSHOT

---
+++ lddi.iso11073.library-1.3.6-SNAPSHOT/readme.txt
@@ -0,0 +1,2 @@
+see universAAL WIKI page at
+http://forge.universaal.org/wiki/lddi:KNX_Integration#Artifact.235_ISO_11073_Library
---
+++ lddi.iso11073.library-1.3.6-SNAPSHOT/src/main/java/org/universAAL/lddi/iso11073/activityhub/devicemodel/SwitchSensor.java
@@ -0,0 +1,70 @@
+package org.universAAL.lddi.iso11073.activityhub.devicemodel;
+
+import org.osgi.service.log.LogService;
+import org.universAAL.lddi.iso11073.activityhub.devicecategory.Iso11073SwitchSensor;
+import org.universAAL.lddi.iso11073.activityhub.devicecategory.ActivityHubDeviceCategoryUtil.ActivityHubDeviceCategory;
+import org.universAAL.lddi.iso11073.activityhub.location.ActivityHubLocationUtil.ActivityHubLocation;
+
+/**
+ * Representation of a switch sensor according to ISO 11073 - 
+ * Part 10471 (Indepentend living activity hub).
+ * 
+ * Specific sensor events (from standard specification):
+ * - switch on
+ * - switch off
+ * - no condition detected (optional)
+ * 
+ * Initially NO_CONDITION_DETECTED is set.
+ * Later, current sensor value can be set to SWITCH_ON and SWITCH_OFF
+ * 
+ * @author Thomas Fuxreiter
+ */
+public class SwitchSensor extends ActivityHubSensor implements Iso11073SwitchSensor{
+
+	private SwitchSensorEvent lastsensorEvent;
+
+	public SwitchSensor(ActivityHubDeviceCategory deviceCategory, 
+			ActivityHubLocation deviceLocation, String deviceId, LogService logger) {
+		super(deviceCategory, deviceLocation, deviceId, logger);
+		
+		// init value is NO_CONDITION_DETECTED
+		this.lastsensorEvent = SwitchSensorEvent.NO_CONDITION_DETECTED;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.universAAL.lddi.iso11073.activityhub.devicemodel.ActivityHubSensor#getSensorEventValue()
+	 */
+	@Override
+	public int getSensorEventValue() {
+		return this.lastsensorEvent.value();
+	}
+
+//	/* (non-Javadoc)
+//	 * @see org.universAAL.lddi.iso11073.activityhub.devicemodel.ActivityHubSensor#setSensorEvent(int)
+//	 */
+//	@Override
+//	public void setSensorEvent(int sensorEvent) {
+//		this.lastsensorEvent = SwitchSensorEvent.getSwitchSensorEvent(sensorEvent);
+//	}
+//
+//	public void setSensorEvent(SwitchSensorEvent sse) {
+//		this.lastsensorEvent = sse;
+//	}
+
+	@Override
+	public void setSensorEventOff() {
+		this.lastSensorEvent = SwitchSensorEvent.SWITCH_OFF;
+		this.sendEvent(SwitchSensorEvent.SWITCH_OFF.value());
+	}
+
+	@Override
+	public void setSensorEventOn() {
+		this.lastSensorEvent = SwitchSensorEvent.SWITCH_ON;
+		this.sendEvent(SwitchSensorEvent.SWITCH_ON.value());
+	}
+
+	public void incomingSensorEvent(int event) {
+		// driver instances must implement this method; device instances not! 
+	}
+
+}
