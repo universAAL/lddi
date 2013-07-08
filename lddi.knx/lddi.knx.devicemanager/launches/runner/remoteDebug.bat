e36418948c680053aa60fb115d9fb93e75ea6e42
DI(FH) Thomas Fuxreiter <thomas.fuxreiter@ait.ac.at>
2013-07-08 09:03:53 +0000

    restructuring lddi repo

--- lddi.knx/lddi.knx.devicemanager/launches/runner/remoteDebug.bat
+++
@@ -1,1 +0,0 @@
-java -Dfelix.config.properties=file:felix/config.ini -Dfelix.system.properties=file:felix/system.properties -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar bundles/org.apache.felix.main_*.jar
