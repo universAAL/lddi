2beb50953b0e857e98f18a61ae00f82639eaa8a0
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-11-20 16:31:56 +0000

    Automatic tag of lddi.knx.devicemanager version: 2.0.3-SNAPSHOT

---
+++ lddi.knx.devicemanager-2.0.3-SNAPSHOT/src/main/java/org/universAAL/lddi/knx/devicemanager/Activator.java
@@ -0,0 +1,55 @@
+/*
+     Copyright 2010-2014 AIT Austrian Institute of Technology GmbH
+	 http://www.ait.ac.at
+     
+     See the NOTICE file distributed with this work for additional
+     information regarding copyright ownership
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+
+package org.universAAL.lddi.knx.devicemanager;
+
+import org.universAAL.lddi.knx.devicemanager.util.LogTracker;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
+
+/***
+ * 
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ *
+ */
+public class Activator implements BundleActivator {
+
+	private LogTracker logTracker;
+	private KnxDeviceManager knxDeviceManager;
+	
+	public void start(BundleContext context) throws Exception {
+		//use a service Tracker for LogService
+		logTracker = new LogTracker(context);
+		logTracker.open();
+		
+		knxDeviceManager = new KnxDeviceManager(context, logTracker);
+	}
+
+
+	public void stop(BundleContext context) throws Exception {
+		// the OSGi framework automatically unregisters any services
+		// registered by this bundle when it is deactivated 
+		// but references of devices in knx.networkdriver must be removed manually
+		knxDeviceManager.stop();
+		this.logTracker.log(LogService.LOG_WARNING,"KnxDeviceManager stopped!");
+	}
+
+}
