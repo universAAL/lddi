d8850b07eca3419c69fd6750dc45b14ed4b60c97
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 16:51:49 +0000

    Automatic tag of lddi.knx.networkdriver version: 3.0.0

---
+++ lddi.knx/lddi.knx.networkdriver/src/test/java/org/universAAL/lddi/knx/networkdriver/test/KnxNetworkdriverFunctionalIT.java
@@ -0,0 +1,183 @@
+package org.universAAL.lddi.knx.networkdriver.test;
+/*
+     Copyright 2010-2014 AIT Austrian Institute of Technology GmbH
+	 http://www.ait.ac.at
+     
+     See the NOTICE file distributed with this work for additional
+     information regarding copyright ownership
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+
+//package org.universAAL.lddi.knx.networkdriver.test;
+//
+//// http://team.ops4j.org/wiki/display/paxexam/Getting+Started+with+OSGi+Tests
+//
+////import static org.junit.Assert.*;
+//
+////import javax.inject.Inject;
+//
+////import org.junit.Test;
+//import org.osgi.framework.Constants;
+////import org.junit.runner.RunWith;
+////import org.ops4j.pax.exam.Configuration;
+////import org.ops4j.pax.exam.Option;
+////import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+//////import org.ops4j.pax.exam.options.FrameworkStartLevelOption;
+////import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+////import org.ops4j.pax.exam.spi.reactors.PerSuite;
+////import org.osgi.framework.BundleContext;
+//////import org.universAAL.lddi.knx.networkdriver.test.util.LogTracker;
+////
+////import static org.ops4j.pax.exam.CoreOptions.*;
+//import org.universAAL.itests.IntegrationTest;
+//import org.universAAL.lddi.knx.networkdriver.Activator;
+//import org.universAAL.lddi.knx.utils.KnxCommand;
+//import org.universAAL.middleware.container.ModuleContext;
+//import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+//import org.universAAL.middleware.container.utils.LogUtils;
+//
+///**
+// * Tests are by default disabled in the main middleware pom file (mw.pom).
+// * To enable them an argument "-DskipTests=false" has to be added to the "mvn" invocation in the command line.
+// * 
+// * @author Thomas Fuxreiter (foex@gmx.at)
+// */
+////@RunWith(JUnit4TestRunner.class)
+////@ExamReactorStrategy(PerSuite.class)
+//public class KnxNetworkdriverFunctionalIT extends IntegrationTest{
+//
+//    public static ModuleContext mc = null;
+//
+//	public KnxNetworkdriverFunctionalIT() {
+//    	setBundleConfLocation("conf");
+//    	setLogLevel(2);
+//
+//    	// set ModuleContext for logging
+//		mc = uAALBundleContainer.THE_CONTAINER
+//				.registerModule(new Object[] { Activator.context });
+//	}
+//
+//    /**
+//     * Helper method for logging.
+//     * 
+//     * @param msg
+//     */
+//	protected void logInfo(String format, Object... args) {
+//		StackTraceElement callingMethod = Thread.currentThread()
+//				.getStackTrace()[2];
+//		LogUtils.logInfo(mc, getClass(), callingMethod
+//				.getMethodName(), new Object[] { formatMsg(format, args) },
+//				null);
+//	}
+//    
+//    /**
+//     * Helper method for logging.
+//     * 
+//     * @param msg
+//     */
+//	protected void logError(Throwable t, String format, Object... args) {
+//		StackTraceElement callingMethod = Thread.currentThread()
+//				.getStackTrace()[2];
+//		LogUtils.logError(mc, getClass(), callingMethod
+//				.getMethodName(), new Object[] { formatMsg(format, args) }, t);
+//	}
+//
+//	/**
+//	 * Helper method: waits until KNX Communication is set up (this is needed
+//	 * because the client starts in a separate thread) and OSGi service is
+//	 * registered.
+//	 */
+//	protected void waitForClient() {
+//		for (int i = 0; i < 20; i++) {
+//			if (Activator.networkDriver.regServiceKnx != null)
+//				break;
+//			try {
+//				Thread.sleep(500);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+//
+//	public void testComposite() {
+//		logAllBundles();
+//	}
+//
+//    /**
+//     * Verifies that runtime platform has correctly started. It prints basic
+//     * information about framework (vendor, version) and lists installed
+//     * bundles.
+//     * 
+//     * @throws Exception
+//     */
+//	public void testOsgiPlatformStarts() throws Exception {
+//		logInfo("FRAMEWORK_VENDOR %s", bundleContext
+//				.getProperty(Constants.FRAMEWORK_VENDOR));
+//		logInfo("FRAMEWORK_VERSION %s", bundleContext
+//				.getProperty(Constants.FRAMEWORK_VERSION));
+//		logInfo("FRAMEWORK_EXECUTIONENVIRONMENT %s", bundleContext
+//				.getProperty(Constants.FRAMEWORK_EXECUTIONENVIRONMENT));
+//
+//		logInfo("!!!!!!! Listing bundles in integration test !!!!!!!");
+//		for (int i = 0; i < bundleContext.getBundles().length; i++) {
+//			logInfo("name: " + bundleContext.getBundles()[i].getSymbolicName());
+//		}
+//		logInfo("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//	}
+//
+////	@Test
+//	public void testKNXCommunication() {
+////		This is done automatically during bundle start
+////		KnxNetworkDriverImp netDrv = new KnxNetworkDriverImp(Activator.context, null); 
+//		
+//		Activator.networkDriver.sendCommand("0/0/2", true, KnxCommand.VALUE_WRITE);		
+//		wait1s();
+//		Activator.networkDriver.requestState("0/0/2");
+//		wait1s();
+//		Activator.networkDriver.sendCommand("0/0/2", false, KnxCommand.VALUE_WRITE);
+//		wait1s();
+//		Activator.networkDriver.requestState("0/0/2");
+//		wait1s();
+//
+//		//requests to knx groupDevice address directly gives no response!!
+//		//Activator.networkDriver.requestState("1.1.5");
+//	}
+//	
+//	void wait1s() {
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+////	private LogTracker logTracker;
+//	
+////	@Inject
+////    private BundleContext bc;
+////	
+////	@Configuration
+////	public Option[] config() {
+////
+////		return options(
+////				workingDirectory("wd"),
+//////				new FrameworkStartLevelOption(5),
+//////				frameworks( felix().version("2.0.1") ),
+////				systemProperty("bundles.configuration.location").value("../conf/services"),
+////				//mavenBundle("com.example.myproject", "myproject-api", "1.0.0-SNAPSHOT"),
+////				//bundle("http://www.example.com/repository/foo-1.2.3.jar"),
+////				junitBundles()); // provisions JUnit and its dependencies as OSGi bundles to the test container.
+////	}
+//
+//}
---
+++ lddi.knx/lddi.knx.networkdriver/src/test/java/org/universAAL/lddi/knx/networkdriver/test/KnxWriterTest.java
@@ -0,0 +1,152 @@
+package org.universAAL.lddi.knx.networkdriver.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.osgi.service.log.LogService;
+import org.universAAL.lddi.knx.networkdriver.Activator;
+import org.universAAL.lddi.knx.networkdriver.KnxNetworkDriverImp;
+import org.universAAL.lddi.knx.networkdriver.KnxWriter;
+import org.universAAL.lddi.knx.networkdriver.util.LogTracker;
+import org.universAAL.lddi.knx.utils.KnxCommand;
+import org.universAAL.lddi.knx.utils.KnxEncoder;
+
+/**
+ * 
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class KnxWriterTest {
+
+	private static InetAddress GROUP_ADDRESS;
+	private static int GROUP_PORT = 3671;
+	private static byte[] sourceByte = new byte[] { 0, 0 };
+	private static int socketTimeout = 3000; // 3s for sending
+	private MulticastSocket mcSocket;
+
+
+	public KnxWriterTest() {
+		
+		try {
+			GROUP_ADDRESS = InetAddress.getByName("224.0.23.12");
+			
+			mcSocket = new MulticastSocket();
+			mcSocket.joinGroup(GROUP_ADDRESS);
+			mcSocket.setSoTimeout(socketTimeout);
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void sendKnxPacket(){
+//		byte[] dataByte = new byte[] {(byte) 0xbc, 0x0d, 0, 0, 0, 2, 1, 0, (byte) 0x81};
+
+//		KnxWriter writer = new KnxWriter(new KnxNetworkDriverImp(Activator.context, null));
+//		new LogTracker(null).log(LogTracker.LOG_INFO, "sendKnxPacket to 0/0/1 ");
+
+		System.out.println("sendKnxPacket to 0/0/2 switch OFF");
+		write("0/0/2", new byte[]{(byte)0x80}, KnxCommand.VALUE_WRITE);
+		wait1s();
+		System.out.println("sendKnxPacket to 0/0/2 switch ON");
+		write("0/0/2", new byte[]{(byte)0x81}, KnxCommand.VALUE_WRITE);
+		wait1s();
+		System.out.println("sendKnxPacket to 0/0/3 switch OFF");
+		write("0/0/3", new byte[]{(byte)0x80}, KnxCommand.VALUE_WRITE);
+		wait1s();
+		System.out.println("sendKnxPacket to 0/0/3 switch ON");
+		write("0/0/3", new byte[]{(byte)0x81}, KnxCommand.VALUE_WRITE);
+	}
+
+	
+	/**
+	 * Send KNX command to UPD multicast channel. Store current sent telegram.
+	 * 
+	 * @param deviceAddress
+	 *            knx group address (1/2/3)
+	 * @param dataByte
+	 *            knx command
+	 * @param commandType
+	 */
+	public void write(String deviceAddress, byte[] dataByte,
+			KnxCommand commandType) {
+//		if (this.lastDeviceAddress != null
+//				&& this.lastDeviceAddress.equals(deviceAddress)
+//				&& Arrays.equals(this.lastDataByte, dataByte)
+//				&& this.lastCommandType == commandType) {
+//			// same command as last time; set Repeat-Bit
+//			this.repeatBit = true;
+//		}
+
+		byte[] telegram = KnxEncoder.encode(false, sourceByte,
+				deviceAddress, dataByte, commandType, true);
+
+		try {
+			// Generating UDP packet
+			DatagramPacket packet = new DatagramPacket(telegram,
+					telegram.length, GROUP_ADDRESS, GROUP_PORT);
+
+			System.out.println("Sending command to KNX: "
+							+ KnxEncoder.convertToReadableHex(telegram));
+
+			// Sending the packet
+			if (mcSocket.isBound()) {
+				// using multicast socket
+				System.out.println("Sending on multicast channel!");
+				mcSocket.send(packet);
+			} else {
+				System.out.println("Not sent on any channel!");
+				throw new Exception(
+						"Unable to write to KNX bus! Socket not bound to destination address!");
+			}
+
+			// store last sent command
+//			this.lastPacketSent = KnxEncoder.removeTrailingZeros(telegram);
+//			this.lastDeviceAddress = deviceAddress;
+//			this.lastDataByte = dataByte;
+//			this.lastCommandType = commandType;
+
+		} catch (IOException e) {
+			System.out.println("Unable to write to KNX bus! " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+//			this.repeatBit = false;
+		}
+	}
+	
+	
+	public void testKNXCommunication() {
+		//		KnxNetworkDriverImp netDrv = new KnxNetworkDriverImp(Activator.context, null); 
+
+		Activator.networkDriver.sendCommand("0/0/2", true, KnxCommand.VALUE_WRITE);		
+		wait1s();
+		Activator.networkDriver.requestState("0/0/2");
+		wait1s();
+		Activator.networkDriver.sendCommand("0/0/2", false, KnxCommand.VALUE_WRITE);
+		wait1s();
+		Activator.networkDriver.requestState("0/0/2");
+		wait1s();
+
+		//requests to knx groupDevice address directly gives no response!!
+		//Activator.networkDriver.requestState("1.1.5");
+	}
+
+	void wait1s() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
