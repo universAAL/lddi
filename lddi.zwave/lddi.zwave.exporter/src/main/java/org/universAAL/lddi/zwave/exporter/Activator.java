fec6741eaa12844e28de804b65feebf0164c04f1
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 16:52:01 +0000

    Automatic tag of lddi.zwave.exporter version: 3.0.0

---
+++ lddi.zwave/lddi.zwave.exporter/src/main/java/org/universAAL/lddi/zwave/exporter/Activator.java
@@ -0,0 +1,72 @@
+/*
+	Copyright 2011-2012 TSB, http://www.tsbtecnologias.es
+	TSB - Tecnolog�as para la Salud y el Bienestar
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.lddi.zwave.exporter;
+
+import java.io.IOException;
+import java.util.Timer;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.universAAL.lddi.zwave.exporter.MotionContact.MotionContactSensorPublisher;
+import org.universAAL.lddi.zwave.exporter.PowerConsumption.PowerReader;
+import org.universAAL.lddi.zwave.exporter.Server.MotionDecoderFactory;
+import org.universAAL.lddi.zwave.exporter.Server.MotionServer;
+import org.universAAL.middleware.container.ModuleContext;
+
+public class Activator implements BundleActivator {
+	public static BundleContext osgiContext = null;
+	public static ModuleContext context = null;
+	private MotionContactSensorPublisher motionPublisher = null;
+	private BundleContext ctx;
+	
+	public void start(BundleContext bcontext) throws Exception {
+		ctx = bcontext;
+		new Thread(){
+			public void run(){   
+				System.out.print("Running power reader \n");
+				Timer t = new Timer();
+				t.schedule(new PowerReader(ctx), 0, 60*1000);
+			}			
+		}.start();
+		new Thread(){
+			public void run(){   
+				System.out.print("Running movement detector\n");
+				motionPublisher = new MotionContactSensorPublisher(ctx);
+				
+				MotionDecoderFactory factory = new MotionDecoderFactory(motionPublisher);
+				MotionServer motionServer = new MotionServer(factory, 53007);
+				try {
+					System.out.print("STARTING SERVER");
+					motionServer.start();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}			
+		}.start();
+		
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		
+	}
+
+}
