1c1ee375ddbb07ffe6bac051ee19f3a6ad7bb7ac
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 16:51:51 +0000

    Automatic tag of lddi.knx.exporter version: 3.0.0

---
+++ lddi.knx/lddi.knx.exporter/src/main/java/org/universAAL/lddi/knx/driver/KnxDpt1Driver.java
@@ -0,0 +1,171 @@
+/*
+     Copyright 2010-2014 AIT Austrian Institute of Technology GmbH
+	 http://www.ait.ac.at
+     
+     See the NOTICE file distributed with this work for additional
+     information regarding copyright ownership
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+
+package org.universAAL.lddi.knx.driver;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.device.Constants;
+import org.osgi.service.device.Device;
+import org.osgi.service.device.Driver;
+import org.osgi.service.log.LogService;
+import org.osgi.util.tracker.ServiceTracker;
+import org.universAAL.lddi.knx.groupdevicecategory.IKnxDpt1;
+import org.universAAL.lddi.knx.groupdevicecategory.KnxGroupDeviceCategoryUtil;
+import org.universAAL.lddi.knx.groupdevicecategory.KnxGroupDeviceCategoryUtil.KnxGroupDeviceCategory;
+import org.universAAL.lddi.knx.groupdevicemodel.KnxDpt1GroupDevice;
+import org.universAAL.lddi.knx.interfaces.IKnxDriverClient;
+
+/**
+ * This Driver class manages driver instances for KNX DPT1 devices.
+ * It is called on new device references coming from OSGi DeviceManager; matching on groupDevice category.
+ * It instantiates drivers for every matching KNX groupDevice.
+ * Attaches exactly one driver instance per deviceId.
+ * Subsequent devices with the same deviceId are rejected!
+ * 
+ * When an attached device service is unregistered:
+ * drivers must take the appropriate action to release this device service
+ * and perform any necessary cleanup, as described in their groupDevice category spec.
+ *  
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class KnxDpt1Driver implements Driver {
+
+	public IKnxDriverClient client;
+	public BundleContext context;
+	public LogService logger;
+	private ServiceRegistration regDriver;
+
+	private static final String MY_DRIVER_ID = "org.universAAL.knx.dpt1.0.0.1";
+	private static final KnxGroupDeviceCategory MY_KNX_DEVICE_CATEGORY = KnxGroupDeviceCategory.KNX_DPT_1; 
+
+	/**
+	 * Management Map of instantiated driver instances.
+	 * Key is groupAddress of the KNX groupDevice
+	 * Value is the associated driver
+	 */
+	public final Map<String, KnxDpt1Instance> connectedDriverInstanceMap = 
+		new ConcurrentHashMap<String, KnxDpt1Instance>();
+	
+	
+	/**
+	 * @param knxManager
+	 * @param context
+	 */
+	public KnxDpt1Driver(IKnxDriverClient client, BundleContext context) {
+		this.client=client;
+		this.context=context;
+		this.logger=client.getLogger();
+		
+		this.registerDriver();
+	}
+
+	
+	/** register this driver in OSGi */
+	private void registerDriver() {
+		Properties propDriver = new Properties();
+		propDriver.put(Constants.DRIVER_ID, MY_DRIVER_ID);
+		this.regDriver=this.context.registerService(Driver.class.getName(), this, 
+				propDriver);
+		
+		if ( this.regDriver != null )
+			this.logger.log(LogService.LOG_INFO, "Driver for KNX-DPT 1.001 registered!");
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see org.osgi.service.device.Driver#match(org.osgi.framework.ServiceReference)
+	 */
+	public int match(ServiceReference reference) throws Exception {
+		// reference = groupDevice service
+		int matchValue = Device.MATCH_NONE;
+		KnxGroupDeviceCategory groupDeviceCategory = null;
+		
+		try {
+			groupDeviceCategory = KnxGroupDeviceCategoryUtil.getCategory(
+					(String)reference.getProperty(Constants.DEVICE_CATEGORY)	);
+		} catch (ClassCastException e) {
+			this.logger.log(LogService.LOG_DEBUG,"Could not cast DEVICE_CATEGORY of requesting"
+					+ " device service " + reference.getProperty(org.osgi.framework.Constants.SERVICE_ID)
+					+ " to String. No match!");
+			return matchValue;
+		}
+		
+		
+		// match check
+		// more possible properties to match: description, serial, id
+		if ( groupDeviceCategory == MY_KNX_DEVICE_CATEGORY ) {
+			matchValue = IKnxDpt1.MATCH_CLASS;
+		} else {
+			this.logger.log(LogService.LOG_DEBUG, "Requesting device service " + groupDeviceCategory +
+					" doesn't match with driver. No match!");
+		}
+		
+		return matchValue; //must be > 0 to match
+	}
+
+	
+	/* (non-Javadoc)
+	 * @see org.osgi.service.device.Driver#attach(org.osgi.framework.ServiceReference)
+	 */
+	public String attach(ServiceReference reference) throws Exception {
+		// get groupAddress
+		KnxDpt1GroupDevice knxDev = (KnxDpt1GroupDevice) this.context.getService(reference);
+
+		if  ( this.connectedDriverInstanceMap.containsKey(knxDev.getGroupAddress()) ) {
+			this.logger.log(LogService.LOG_WARNING, "There is already a driver instance available for " +
+					" the groupDevice " + knxDev.getGroupAddress());
+			return "driver already exists for this groupDevice!";
+		}
+		
+		// create "driving" instance
+		KnxDpt1Instance instance = new KnxDpt1Instance(this);
+
+		// store instance
+		this.connectedDriverInstanceMap.put(knxDev.getGroupAddress(), instance);
+
+		// init service tracker; the driver instance itself tracks on the groupDevice reference!
+		ServiceTracker tracker = new ServiceTracker(this.context, reference, instance);
+		tracker.open();
+
+		return null;
+	}
+
+	
+	/**
+	 * delete instance references 
+	 * unregister my services ?
+	 */
+	public void stop() {
+		
+		// delete instance references !!
+		for ( Iterator<KnxDpt1Instance> it = this.connectedDriverInstanceMap.values().iterator(); it.hasNext(); ) {
+			it.next().detachDriver();
+		}
+
+		// the managed service and driver service is unregistered automatically by the OSGi framework!
+	}
+}
---
+++ lddi.knx/lddi.knx.exporter/src/main/java/org/universAAL/lddi/knx/driver/KnxDpt3Instance.java
@@ -0,0 +1,171 @@
+/*
+     Copyright 2010-2014 AIT Austrian Institute of Technology GmbH
+	 http://www.ait.ac.at
+     
+     See the NOTICE file distributed with this work for additional
+     information regarding copyright ownership
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+ */
+
+package org.universAAL.lddi.knx.driver;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.device.Constants;
+import org.osgi.service.log.LogService;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.universAAL.lddi.knx.groupdevicecategory.IKnxDpt3;
+import org.universAAL.lddi.knx.groupdevicemodel.KnxDpt3GroupDevice;
+import org.universAAL.lddi.knx.interfaces.IKnxReceiveMessage;
+import org.universAAL.lddi.knx.interfaces.KnxDriver;
+
+/**
+ * Working instance of the IKnxDpt3 driver. Registers a service/device in OSGi
+ * registry. Tracks on the KNX groupDevice service passed in the attach method in
+ * KnxDpt3Driver class. This instance is passed to the consuming client (e.g.
+ * uAAL exporter bundle). When the KNX groupDevice service disappears, this driver is
+ * removed from the consuming client and detached from the groupDevice.
+ * 
+ * This driver handles knx 4-bit unsigned int events (knx datapoint 3), which is
+ * dimming step and blind step.
+ * 
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public class KnxDpt3Instance extends KnxDriver implements IKnxDpt3, IKnxReceiveMessage,
+		ServiceTrackerCustomizer, Constants {
+
+	private BundleContext context;
+	private LogService logger;
+	private KnxDpt3Driver parent;
+
+	/**
+	 * @param c
+	 *            OSGi BundleContext
+	 * @param sr
+	 *            Service reference of KNX groupDevice
+	 * @param client
+	 *            Link to consumer of this driver (e.g. uAAL exporter bundle)
+	 */
+	public KnxDpt3Instance(KnxDpt3Driver parent_) {
+		// BundleContext context, IKnxDriverClient client,
+		// LogService logger) {
+		super(parent_.client);
+
+		this.parent = parent_;
+		this.context = parent_.context;
+		this.logger = parent_.logger;
+	}
+
+	/**
+	 * Empty constructor for Unit Tests.
+	 */
+	public KnxDpt3Instance() {
+	};
+
+	/**
+	 * track on my groupDevice
+	 * 
+	 * @param IKnxDpt3
+	 *            groupDevice service
+	 * @return The service object to be tracked for the ServiceReference object
+	 *         or null if the ServiceReference object should not be tracked.
+	 */
+	public Object addingService(ServiceReference reference) {
+		KnxDpt3GroupDevice knxDev = (KnxDpt3GroupDevice) this.context
+				.getService(reference);
+
+		if (knxDev == null)
+			this.logger.log(LogService.LOG_ERROR,
+					"knxDev is null for some reason!");
+
+		/** now couple my driver to the groupDevice */
+		if (this.setgroupDevice(knxDev))
+			this.logger.log(LogService.LOG_INFO, "Successfully coupled "
+					+ IKnxDpt3.MY_DEVICE_CATEGORY + " driver to groupDevice "
+					+ this.groupDevice.getGroupDeviceId());
+		else {
+			this.logger.log(LogService.LOG_ERROR, "Error coupling "
+					+ IKnxDpt3.MY_DEVICE_CATEGORY + " driver to groupDevice "
+					+ this.groupDevice.getGroupDeviceId());
+			return null;
+		}
+
+		// JavaDoc: @return The service object to be tracked for the
+		// ServiceReference object or null if the ServiceReference object should
+		// not be tracked.
+		return knxDev;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.util.tracker.ServiceTrackerCustomizer#modifiedService(org.osgi
+	 * .framework.ServiceReference, java.lang.Object)
+	 */
+	public void modifiedService(ServiceReference reference, Object service) {
+		this.logger.log(LogService.LOG_INFO,
+				"Tracked knx groupDevice service was modified. "
+						+ "Going to update the KnxDpt3Instance");
+		removedService(reference, service);
+		addingService(reference);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.util.tracker.ServiceTrackerCustomizer#removedService(org.osgi
+	 * .framework.ServiceReference, java.lang.Object)
+	 */
+	public void removedService(ServiceReference reference, Object service) {
+		// removed groupDevice service
+		this.context.ungetService(reference);
+		this.detachDriver();
+		this.removeDriver();		
+		
+//		KnxDpt3GroupDevice knxDev = (KnxDpt3GroupDevice) this.context.getService(reference);
+		KnxDpt3GroupDevice knxDev = (KnxDpt3GroupDevice) service;
+		this.parent.connectedDriverInstanceMap.remove(knxDev.getGroupAddress());
+	}
+	
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.universAAL.lddi.knx.interfaces.IKnxReceiveMessage#
+	 * newMessageFromKnxBus(byte[])
+	 */
+	public void newMessageFromKnxBus(byte[] event) {
+
+		//String code = calculateStepCode(event[0]);
+		String code = KnxDpt3GroupDevice.calculateStepCode(event[0], this.groupDevice.getDatapointTypeSubNumber());
+				
+		if (code.isEmpty()) {
+			this.logger.log(LogService.LOG_WARNING, "Driver " + IKnxDpt3.MY_DEVICE_CATEGORY + " for groupDevice " + 
+					this.groupDevice.getGroupAddress() + " says: knx datapoint type " + this.groupDevice.getDatapointType() +
+					" is not implemented!");
+			return;
+		}
+			
+		this.logger.log(LogService.LOG_INFO, "Driver " + IKnxDpt3.MY_DEVICE_CATEGORY + " for groupDevice " + 
+				this.groupDevice.getGroupAddress() + " with knx datapoint type " + this.groupDevice.getDatapointType() +
+				" received new step code " + code );
+		
+		this.client.incomingSensorEvent( this.groupDevice.getGroupAddress(), 
+				this.groupDevice.getDatapointTypeMainNumber(), this.groupDevice.getDatapointTypeSubNumber(),
+				code);
+	}
+
+}
