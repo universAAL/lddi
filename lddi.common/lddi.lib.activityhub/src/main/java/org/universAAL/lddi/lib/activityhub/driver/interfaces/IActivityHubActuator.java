92082f943e9afb921260863178ca6a7e97317a8b
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 09:55:51 +0000

    Automatic tag of lddi.lib.activityhub version: 3.1.0

---
+++ lddi.common/lddi.lib.activityhub/src/main/java/org/universAAL/lddi/lib/activityhub/driver/interfaces/IActivityHubActuator.java
@@ -0,0 +1,38 @@
+/*
+     Copyright 2010-2014 AIT Austrian Institute of Technology GmbH
+	 http://www.ait.ac.at
+     
+     See the NOTICE file distributed with this work for additional
+     information regarding copyright ownership
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+
+package org.universAAL.lddi.lib.activityhub.driver.interfaces;
+
+/**
+ * An actuator device is controllable. It should implement this interface.
+ *  
+ * @author Thomas Fuxreiter (foex@gmx.at)
+ */
+public interface IActivityHubActuator {
+	/***
+	 * The specific drivers instances have to implement this method
+	 * to receive events from the consuming bundles (e.g. uAAL context bus events) 
+	 * 
+	 * @param deviceAddress  address of the device or the group that fire the message
+	 * @param message array of byte containing the information of the status or command
+	 */
+	public abstract void newActuatorCommand(String deviceId,byte[] message);
+	
+}
